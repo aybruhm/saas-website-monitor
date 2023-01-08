91c3cbf4dfcf96e8cfb4d72c724e080de3b1a06e
Abram <israelvictory87@gmail.com>
2023-01-08 02:02:27 +0100

    Feat: register models to admin site

--- apps/monitor/admin.py
+++ apps/monitor/admin.py
@@ -1,3 +1,25 @@
+# Django Imports
 from django.contrib import admin
 
-# Register your models here.
+# Own Imports
+from apps.monitor.models import Websites, HistoricalStats, People, NotifyGroup
+
+
+@admin.register(Websites)
+class WebsitesAdmin(admin.ModelAdmin):
+    list_display = ["site", "date_created"]
+
+
+@admin.register(HistoricalStats)
+class HistoricalStatsAdmin(admin.ModelAdmin):
+    list_display = ["track", "up_counts", "down_counts", "date_created"]
+
+
+@admin.register(People)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ["email_address", "date_created"]
+
+
+@admin.register(NotifyGroup)
+class NotifyGroupAdmin(admin.ModelAdmin):
+    list_display = ["name", "notify", "date_created"]
