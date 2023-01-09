6570554171c16a2da93ebf9121149f8ca72bed05
Abram <israelvictory87@gmail.com>
2023-01-09 02:18:42 +0100

    Feat: implemented get logs of historical stats api view

--- apps/monitor/views.py
+++ apps/monitor/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# Rest Framework Imports
+from rest_framework import generics, status
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+# Own Imports
+from apps.monitor.models import HistoricalStats
+from apps.monitor.serializers import HistoricalStatsSerializer
+
+
+class GetLogsOfHistoricalStatsAPIView(generics.GenericAPIView):
+
+    serializer_class = HistoricalStatsSerializer
+    queryset = HistoricalStats.objects.select_related("track")
+
+    def get(self, request: Request) -> Response:
+        historical_stats = self.get_queryset()
+        serializer = self.serializer_class(historical_stats, many=True)
+        return Response(
+            {
+                "message": "Log of historical stats retrieved!",
+                "data": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
