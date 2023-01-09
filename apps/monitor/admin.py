ee00581a6f38f0f297fc8572157f5d6b30e99cb0
Abram <israelvictory87@gmail.com>
2023-01-09 21:17:42 +0100

    Update: created authentication schema admin

--- apps/monitor/admin.py
+++ apps/monitor/admin.py
@@ -2,17 +2,33 @@
 from django.contrib import admin
 
 # Own Imports
-from apps.monitor.models import Websites, HistoricalStats, People, NotifyGroup
+from apps.monitor.models import (
+    Websites,
+    AuthenticationSchema,
+    HistoricalStats,
+    People,
+    NotifyGroup,
+)
 
 
 @admin.register(Websites)
 class WebsitesAdmin(admin.ModelAdmin):
-    list_display = ["site", "date_created"]
+    list_display = ["site", "has_authentication", "date_created"]
+
+
+@admin.register(AuthenticationSchema)
+class AuthenticationSchemaAdmin(admin.ModelAdmin):
+    list_display = ["id", "basic_auth", "bearer_auth", "date_created"]
 
 
 @admin.register(HistoricalStats)
 class HistoricalStatsAdmin(admin.ModelAdmin):
-    list_display = ["track", "uptime_counts", "downtime_counts", "date_created"]
+    list_display = [
+        "track",
+        "uptime_counts",
+        "downtime_counts",
+        "date_created",
+    ]
 
 
 @admin.register(People)
