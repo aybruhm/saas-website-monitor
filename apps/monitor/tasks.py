c779b75276d580b96609ac3520156c57cdfcdfc3
Abram <israelvictory87@gmail.com>
2023-01-09 02:00:43 +0100

    Update: modified notify_group_of_people_via_email

--- apps/monitor/tasks.py
+++ apps/monitor/tasks.py
@@ -10,12 +10,14 @@ from celery import shared_task
 
 
 @shared_task(max_retries=3)
-def notify_group_of_people_via_email(website: str):
+def notify_group_of_people_via_email(website: str) -> str:
     """
     This function notifies a group of people via email when a website is down.
 
     :param website: str = The website that is currently facing a downtime
     :type website: str
+    
+    :return str: A message
     """
 
     group_emails = list(
@@ -29,5 +31,6 @@ def notify_group_of_people_via_email(website: str):
         message=f"Hello, {website} is currently facing a downtime.",
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=group_emails,
-        fail_silently=True,
     )
+    
+    return "Mail sent successfully!"
