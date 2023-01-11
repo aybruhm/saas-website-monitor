691df5641c04384a9561cc21c2cbbf557b2882d1
Abram <israelvictory87@gmail.com>
2023-01-11 01:26:48 +0100

    Feat: Implementedadd website and authentication types api view

--- apps/monitor/views.py
+++ apps/monitor/views.py
@@ -4,8 +4,34 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 # Own Imports
-from apps.monitor.models import HistoricalStats
-from apps.monitor.serializers import HistoricalStatsSerializer
+from apps.monitor.models import HistoricalStats, AuthTypes
+from apps.monitor.serializers import (
+    HistoricalStatsSerializer,
+    WebsiteSerializer,
+)
+
+
+class AuthenticationTypesAPIView(generics.ListAPIView):
+    def get(self, request: Request) -> Response:
+        auth_types = AuthTypes.choices
+        data = [{"name": auth[0]} for auth in auth_types]
+        return Response(data=data, status=status.HTTP_200_OK)
+
+
+class AddWebsiteAPIView(generics.CreateAPIView):
+
+    serializer_class = WebsiteSerializer
+
+    def post(self, request: Request) -> Response:
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        response = {
+            "message": "Website to monitor added!",
+            "data": serializer.data,
+        }
+        return Response(data=response, status=status.HTTP_201_CREATED)
 
 
 class GetLogsOfHistoricalStatsAPIView(generics.GenericAPIView):
