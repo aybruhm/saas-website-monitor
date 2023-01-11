68c9e2750119c13dfb2e152be18ded9591cd3e9e
Abram <israelvictory87@gmail.com>
2023-01-11 01:12:36 +0100

    Feat: Implemented website serializer, validate auth types and ensure auth types requires authentication flag methods

--- apps/monitor/serializers.py
+++ apps/monitor/serializers.py
@@ -1,8 +1,47 @@
+# Stdlib Imports
+from typing import OrderedDict
+
 # Rest Framework Imports
 from rest_framework import serializers
 
 # Own Imports
-from apps.monitor.models import HistoricalStats
+from apps.monitor.models import HistoricalStats, Websites, AuthTypes
+
+
+class WebsiteSerializer(serializers.ModelSerializer):
+
+    auth_types = serializers.CharField(required=False)
+    has_authentication = serializers.BooleanField(default=False)
+
+    class Meta:
+        model = Websites
+        fields = ["site", "auth_types", "has_authentication"]
+
+    def validate_auth_types(self, value: str) -> str:
+        auth_types = [auth[0] for auth in AuthTypes.choices]
+
+        if value not in auth_types:
+            raise serializers.ValidationError(
+                {"message": "Authentication type not supported."}
+            )
+        return value
+
+    def _ensure_auth_types_requires_authentication_flag(
+        self, authenticated: bool, auth_type: str
+    ):
+        if authenticated and not auth_type:
+            raise serializers.ValidationError(
+                {"message": "Authentication type is required."}
+            )
+
+    def create(self, validated_data: OrderedDict) -> Websites:
+        # validation to ensure that auth types
+        # requires authentication to be True
+        self._ensure_auth_types_requires_authentication_flag(
+            validated_data["has_authentication"],
+            validated_data["auth_types"],
+        )
+        return super().create(validated_data)
 
 
 class HistoricalStatsSerializer(serializers.ModelSerializer):
