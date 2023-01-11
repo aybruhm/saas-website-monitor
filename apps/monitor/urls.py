1e4d088228ba416817a8c08be8cb2ba46ab55edf
Abram <israelvictory87@gmail.com>
2023-01-11 01:27:07 +0100

    Feat: created add website and authentication types api endpoint

--- apps/monitor/urls.py
+++ apps/monitor/urls.py
@@ -1,16 +1,26 @@
 # Django Imports
-from django.urls import reverse, path
+from django.urls import path
 
 # Own Imports
-from apps.monitor.views import GetLogsOfHistoricalStatsAPIView
+from apps.monitor.views import (
+    AuthenticationTypesAPIView,
+    AddWebsiteAPIView,
+    GetLogsOfHistoricalStatsAPIView,
+)
 
 
 app_name = "monitor"
 
 urlpatterns = [
     path(
+        "authentication-types/",
+        AuthenticationTypesAPIView.as_view(),
+        name="auth_types",
+    ),
+    path("add-website/", AddWebsiteAPIView.as_view(), name="add_website"),
+    path(
         "historical-stats/",
         GetLogsOfHistoricalStatsAPIView.as_view(),
         name="historical_stats",
-    )
+    ),
 ]
