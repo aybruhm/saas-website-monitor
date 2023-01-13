305c9a5b17d7b890c2aeca738ad6c418a4639987
Abram <israelvictory87@gmail.com>
2023-01-13 01:15:08 +0100

    Refactor: update services

--- apps/monitor/services.py
+++ apps/monitor/services.py
@@ -1,13 +1,3 @@
-# Stdlib Imports
-from typing import List
-
-# Django Imports
-from django.db import transaction
-
-# Own Imports
-from apps.monitor.selectors import get_historical_stats
-from apps.monitor.tasks import notify_group_of_people_via_email
-
 # Third Party Imports
 import httpx
 
@@ -76,38 +66,3 @@ def bearer_authentication(website: str, data: dict) -> str:
         jwt_token = response.json()["access"]
     return jwt_token
 
-
-@transaction.atomic
-def monitor_websites(websites: List[str]) -> str:
-    """
-    This function checks if the website is up or down,
-    and if it's down, it sends an email to a group of people.
-
-    :param websites: List[str] -> This is the list of websites that we want to monitor
-    :type websites: List[str]
-
-    :return: A string of message.
-    """
-
-    with httpx.Client() as client:
-        for website in websites:
-            response, historial_stats = client.get(
-                website
-            ), get_historical_stats(website)
-
-            if response.status_code == 200:
-
-                # save up time to db
-                historial_stats.uptime_counts += 1
-                historial_stats.save(update_fields=["uptime_counts"])
-                return "Uptime counts has increased with 1."
-
-            elif response.status_code in [500, 502, 503, 504]:
-
-                # save down time to db
-                historial_stats.downtime_counts += 1
-                historial_stats.save(update_fields=["downtime_counts"])
-
-                # send mail to group
-                notify_group_of_people_via_email.delay(website)
-                return "Downtime counts has increased with 1."
