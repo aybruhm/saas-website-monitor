73d54cd6c459430946d15354b9dbd0684a9996c3
Abram <israelvictory87@gmail.com>
2023-01-13 12:05:49 +0100

    Update: modified monitor_websites_up_x_downtimes task

--- apps/monitor/tasks.py
+++ apps/monitor/tasks.py
@@ -1,13 +1,10 @@
-# Stdlib Imports
-from typing import List
-
 # Django Imports
 from django.conf import settings
 from django.db import transaction
 from django.core.mail import send_mail as send_mail_to_group, get_connection
 
 # Own Imports
-from apps.monitor.models import NotifyGroup, StatusTypes
+from apps.monitor.models import NotifyGroup, StatusTypes, Websites
 from apps.monitor.selectors import get_historical_stats, get_website
 from apps.monitor.tasks import notify_group_of_people_via_email
 
@@ -54,23 +51,25 @@ def notify_group_of_people_via_email(website: str) -> str:
 
 @transaction.atomic
 @shared_task(max_tries=3)
-def monitor_websites_up_x_downtimes(websites: List[str]) -> str:
+def monitor_websites_up_x_downtimes() -> str:
     """
     This function checks if the website is up or down,
     and if it's down, it sends an email to a group of people.
-
-    :param websites: List[str] -> This is the list of websites that we want to monitor
-    :type websites: List[str]
 
     :return: A string of message.
     """
 
     with httpx.Client() as client:
-        for website in websites:
+        for website in list(Websites.objects.values_list("site", flat=True)):
+            
+            # get the response of the website,
+            # and the historical stats of the website
             response, historial_stats = (
                 client.get(website),
                 get_historical_stats(website),
             )
+            
+            # get the site object
             site = get_website(website)
 
             if response.status_code == 200:
