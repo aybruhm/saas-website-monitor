1fe4ab59cf4d0ede4c64ee66f6bc4bd73de83c07
Abram <israelvictory87@gmail.com>
2023-01-13 18:42:44 +0100

    Update: added static_root dir

--- saas/settings.py
+++ saas/settings.py
@@ -144,6 +144,7 @@ DEFAULT_FROM_EMAIL = "noreply@monitor.test"
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
